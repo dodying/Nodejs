ae432e1020193578309f8fb0d019af2531ae1f42
dodying <549779963@qq.com>
2018-02-20 22:12:50 +0800

    update checkExistSever

--- checkExistSever/index.js
+++ checkExistSever/index.js
@@ -7,7 +7,7 @@
 // @Author:             dodying
 // @Date:               2018-02-16 12:49:33
 // @Last Modified by:   dodying
-// @Last Modified time: 2018-02-18 17:15:13
+// @Last Modified time: 2018-02-20 20:04:39
 // @Namespace:          https://github.com/dodying/Nodejs
 // @SupportURL:         https://github.com/dodying/Nodejs/issues
 // @Require:            express,body-parser
@@ -18,12 +18,12 @@ const libraryFolder = 'E:\\F\\';
 const esPath = 'D:\\GreenSoftware\\Everything\\es.exe';
 
 //native modules
-const fs = require('fs');
 const cp = require('child_process');
 
 //3rd party modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const async = require('async');
 
 //
 const escape = text => text.replace(/[\\/:*?"<>|]/g, '-').replace(/\.$/, '');
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', function(req, res) {
-  console.log('GET /')
+  console.log('GET /');
   let html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
   res.writeHead(200, {
     'Content-Type': 'text/html;charset=utf-8'
@@ -43,16 +43,37 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  let parm = req.body.name;
-  //let lst = cp.execSync(`${esPath} -p "${libraryFolder}" /a-d -name "${escape(parm)}"`).toString().split(/[\r\n]+/).filter(i => i);
-  cp.exec(`${esPath} -p "${libraryFolder}" /a-d -name "${escape(parm)}"`, (error, stdout, stderr) => {
-    let lst = stdout.split(/[\r\n]+/).filter(i => i);
-    console.log('POST /', req.body, lst);
-    res.writeHead(200, {
-      'Content-Type': 'application/json;charset=utf-8'
+  if ('name' in req.body) {
+    cp.exec(`${esPath} -p "${libraryFolder}" /a-d -name "${escape(req.body.name)}"`, (error, stdout, stderr) => {
+      let lst = stdout.split(/[\r\n]+/).filter(i => i);
+      console.log('POST /', req.body, lst);
+      res.writeHead(200, {
+        'Content-Type': 'application/json;charset=utf-8'
+      });
+      res.end(JSON.stringify(lst));
     });
-    res.end(JSON.stringify(lst));
-  });
+  } else if ('names' in req.body) {
+    console.log('POST /', req.body);
+    async.mapSeries(JSON.parse(req.body.names), (name, cb) => {
+      name = name.split(',').map(i => `-name "${escape(i)}"`).join(' ');
+      cp.exec(`${esPath} -p "${libraryFolder}" /a-d ${name}`, (error, stdout, stderr) => {
+        let lst = stdout.split(/[\r\n]+/).filter(i => i);
+        cb(null, lst);
+      });
+    }, (err, results) => {
+      if (err) console.error(err);
+      let obj = {};
+      results.forEach((i, j) => {
+        obj[j] = i;
+      });
+      console.log('POST /', obj);
+      res.writeHead(200, {
+        'Content-Type': 'application/json;charset=utf-8'
+      });
+      res.end(JSON.stringify(obj));
+    });
+
+  }
 });
 
 port = 3000;
