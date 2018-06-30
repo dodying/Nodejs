096b9e30bea237da52a674ee340458a538bbc965
dodying <549779963@qq.com>
2018-06-30 20:26:50 +0800

    1. 增加get

--- checkExistSever/index.js
+++ checkExistSever/index.js
@@ -7,7 +7,7 @@
 // @Author:             dodying
 // @Date:               2018-02-16 12:49:33
 // @Last Modified by:   dodying
-// @Last Modified time: 2018-04-05 19:30:18
+// @Last Modified time: 2018-06-06 18:08:43
 // @Namespace:          https://github.com/dodying/Nodejs
 // @SupportURL:         https://github.com/dodying/Nodejs/issues
 // @Require:            express,body-parser
@@ -25,6 +25,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const async = require('async');
 
+
 //
 const escape = text => text.replace(/[\\/:*?"<>|]/g, '-').replace(/\.$/, '');
 
@@ -33,31 +34,53 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', ' 3.2.1');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
 app.get('/', function(req, res) {
-  console.log('GET /');
-  let html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-  res.writeHead(200, {
-    'Content-Type': 'text/html;charset=utf-8'
-  });
-  res.end(html);
+  if ('name' in req.query) {
+    cp.exec(`${esPath} -size -p "${libraryFolder}" /a-d -name "${escape(req.query.name)}" -n 20`, (error, stdout, stderr) => {
+      let lst = stdout.split(/[\r\n]+/).map(i => i.replace(/^\s+/g, '')).filter(i => i);
+      console.log('GET /', req.query, lst);
+      res.writeHead(200, {
+        'Content-Type': 'application/json;charset=utf-8'
+      });
+      res.end(JSON.stringify(lst, null, 2));
+    });
+  } else {
+    console.log('GET /');
+    let html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+    res.writeHead(200, {
+      'Content-Type': 'text/html;charset=utf-8'
+    });
+    res.end(html);
+  }
+
 });
 
 app.post('/', function(req, res) {
   if ('name' in req.body) {
     cp.exec(`${esPath} -size -p "${libraryFolder}" /a-d -name "${escape(req.body.name)}" -n 20`, (error, stdout, stderr) => {
-      let lst = stdout.split(/[\r\n]+/).filter(i => i);
+      let lst = stdout.split(/[\r\n]+/).map(i => i.replace(/^\s+/g, '')).filter(i => i);
       console.log('POST /', req.body, lst);
       res.writeHead(200, {
         'Content-Type': 'application/json;charset=utf-8'
       });
-      res.end(JSON.stringify(lst));
+      res.end(JSON.stringify(lst, null, 2));
     });
   } else if ('names' in req.body) {
-    console.log('POST /', req.body);
-    async.mapSeries(JSON.parse(req.body.names), (name, cb) => {
+    let body = JSON.parse(req.body.names);
+    console.log('POST /', JSON.stringify(body, null, 2));
+    async.mapLimit(body, 3, (name, cb) => {
       name = name.split(',').map(i => `-name "${escape(i)}"`).join(' ');
       cp.exec(`${esPath} -size -p "${libraryFolder}" /a-d ${name} -n 20`, (error, stdout, stderr) => {
-        let lst = stdout.split(/[\r\n]+/).filter(i => i);
+        let lst = stdout.split(/[\r\n]+/).map(i => i.replace(/^\s+/g, '')).filter(i => i);
         cb(null, lst);
       });
     }, (err, results) => {
@@ -66,11 +89,12 @@ app.post('/', function(req, res) {
       results.forEach((i, j) => {
         obj[j] = i;
       });
-      console.log('POST /', obj);
+      obj = JSON.stringify(obj, null, 2);
+      console.log('RESPONSE /', obj);
       res.writeHead(200, {
         'Content-Type': 'application/json;charset=utf-8'
       });
-      res.end(JSON.stringify(obj));
+      res.end(obj);
     });
 
   }
