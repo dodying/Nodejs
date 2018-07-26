0083e3b961f638b3d21d624be00de743b1e2b35f
dodying <549779963@qq.com>
2018-07-26 17:07:12 +0800

    1. 新增功能: 删除宣传图

--- comicSort/config.js
+++ comicSort/config.js
@@ -1,17 +1,19 @@
 'use strict'
 
 let config = {
-  'globRecursive': false,
-  'comicFolder': 'F:\\Temp',
-  'libraryFolder': 'F:\\ComicLibrary',
-  'jTitle': false,
-  'checkImageSize': true,
-  'rateD': 1.1,
-  'rateS': 0.9,
-  'sizeD': 1280,
-  'sizeS': 780,
-  'cover': true,
-  'subFolder': [
+  '7z': 'D:\\GreenSoftware\\_Basis\\7-Zip\\7z.exe',
+  proxy: 'http://127.0.0.1:2346',
+  globRecursive: true,
+  comicFolder: 'F:\\Temp',
+  libraryFolder: 'F:\\ComicLibraryNew',
+  jTitle: false,
+  delIntroPic: true,
+  introPic: [ '01c9b75a4f', '047379e168', '04c7df9318', '08f78aed5a', '096712ffd7', '09fe6b9a55', '0b34a8553b', '0cebce8abd', '0de93ecf0d', '0ec8fc9b72', '0f480f45a4', '0fe76e7ef3', '10027a8e65', '10970b069b', '10f227dc03', '11c7c2baf0', '13536e68df', '153267b08d', '17f9d9ba37', '1877d56115', '1c6a1e52af', '1db1203303', '1e402f0f53', '1edeab5112', '1f3ba78557', '1f9f5f0455', '218a909563', '21eb59053f', '220fe636f3', '22b1d4ebd4', '232e2a58dd', '2494482757', '26a6238844', '2716ad4d6c', '279244b5d9', '27dddf4c0c', '28c583d537', '296fdd3d46', '2dbecc067f', '2f9c7c93bf', '314e942ff8', '327579d54b', '3590841471', '35db0e958e', '3844b1cb1e', '3d0ad385e2', '3de6720c2e', '3e2d10e613', '40c68fca53', '42f952f114', '43067d768b', '4354346b0f', '45019d91e7', '4812b50afe', '4a1e4ec359', '4b06c068a9', '4d6b8506d3', '4dbde53805', '51991d0bb9', '51e3b40649', '5230467b48', '5512077961', '561595013b', '57099854b7', '5c0cb48a81', '5c1f1558c1', '5c6d42c811', '5d0770c2f4', '5ebea5e33d', '5f24a9d3e5', '5fe30304c3', '6215ff4e19', '62c4cab030', '63b5c215bb', '64209bac21', '648ec7d480', '65a2c9cafa', '683f48d8e8', '694f58e45e', '696e67202c', '69ff83db2a', '6a1f841bfe', '6ac16b45ca', '6b04214926', '6d58eb8ec6', '6e56a2ba2f', '6fb883e08e', '7070783c22', '709d72526d', '7155d6da0c', '724b736b85', '72cde6d709', '7374ce0813', '76cace658d', '76eb58fb06', '777b84d450', '78500842f0', '7a177d153c', '7a326e8812', '7b88fccc0b', '7ba38927f1', '7f7daa231f', '80d6e2739c', '80f82ac025', '81e88b4743', '8459f073c9', '8575c161e3', '88a81a5919', '8b7d43e713', '8c6cdc818b', '8ca2275327', '8e21688aff', '9108df6263', '91e1921760', '9219a21b5c', '936123901c', '94149929c5', '9514cd71b0', '975d300af2', '9962ef2da0', '99a5b3329c', '9a2db889fc', '9bf2c60988', '9c886361b7', '9d1afcfa9f', '9fd0569cf0', 'a067351faf', 'a270087bf4', 'a34aedff53', 'a4c62781b7', 'a4f14d384e', 'a52efe6a52', 'a559f2a2ce', 'a63cb2f101', 'a90d4d4842', 'aac5a755cf', 'ab31dc6dfc', 'ac47b07a80', 'ad6eb5f7f6', 'ae82a2990c', 'aeda509902', 'af3b48d59b', 'b290f6796b', 'b3b922542f', 'b40da3921e', 'b52e58da52', 'b538de4398', 'b5f93ceea4', 'b7d79f4b9a', 'b7e4932d52', 'b9a2f6e4fa', 'bb8cb1f8a3', 'bb956b36f4', 'bd42c475b7', 'be3393a4f7', 'be990c6fd3', 'c2ad27379a', 'c30fe49512', 'c3ae070961', 'c493e654e5', 'c78220c4f3', 'c7a88f9784', 'c8d2ff755b', 'c9e23490e9', 'cb33309296', 'cba27b6c5b', 'cc480dfa5c', 'cea1eed92a', 'cee7227cc5', 'cf5e9f826d', 'd1b3f69da1', 'd226489fc4', 'd2f32d160c', 'd408cc8bcd', 'd5e5719bbb', 'd6195cd4d0', 'd7286fd2cc', 'd7436f77a6', 'd7d55f85cc', 'dbb6accf9a', 'dc4461c674', 'dc81823121', 'ddd81dc53a', 'df536ccecc', 'e05c770585', 'e0e8b6e529', 'e21670f4e7', 'e342bc5b98', 'e4e4abf3ff', 'e4f2df0dd0', 'e56892250c', 'e5ed033033', 'e7db8c9963', 'e88dab0d1f', 'e8d095da2c', 'ebd7ed2c62', 'ec5af0fc50', 'edba62ab8e', 'ee471c07b3', 'ee9af0c06d', 'eec614534a', 'f08b54c152', 'f0a803e94b', 'f727a96e54', 'f753701608', 'f79eac3b6d', 'f8c8ea8eb4', 'fa71e5ba84', 'fa881d015c', 'fb2b3a3034', 'fb336d9e48', 'fb6a20ffc7', 'fb71027a31', 'fd8c04296d', 'ff0992e487', 'ffa6cd83f9' ],
+  checkImageSize: true,
+  rate: 1,
+  size: 780,
+  cover: true,
+  subFolder: [
     '0.Series',
     '1.Cosplay',
     '2.Image Set',
@@ -24,9 +26,9 @@ let config = {
     '9.Artist',
     '10.Other'
   ],
-  'subFolderDelete': 'X.Deleted',
-  'specialFolder': '#Star',
-  'specialRule': [
+  subFolderDelete: 'X.Deleted',
+  specialFolder: '#Star',
+  specialRule: [
     {
       'misc': 'webtoon',
       'folder': '[Webtoon]'
@@ -75,6 +77,7 @@ let config = {
       'folder': 'illumination'
     }, {
       'group': 'moonphase',
+      'artist': 'yuran',
       'folder': '[彩图]MoonPhase'
     }, {
       'group': 'redrop',
@@ -92,7 +95,7 @@ let config = {
       'artist': 'fujisaki hikari',
       'folder': '[萝莉]藤崎ひかり'
     }, {
-      'artist': 'shousan bouzu | shioyama bou',
+      'artist': 'shousan bouzu',
       'folder': '[巨乳萝莉]しょうさん坊主'
     }, {
       'artist': 'katsurai yoshiaki',
@@ -197,7 +200,7 @@ let config = {
       'artist': 'azuma yuki',
       'folder': '[萝莉]azuma yuki'
     }, {
-      'artist': 'azuma yuki | mitsubishi mikoto',
+      'artist': 'azuma yuki',
       'folder': '[萝莉]azuma yuki'
     }, {
       'artist': 'rubi-sama',
@@ -221,9 +224,6 @@ let config = {
       'artist': 'kinnotama',
       'folder': '[萝莉]kinnotama'
     }, {
-      'artist': 'tachibana omina | ominaeshi',
-      'folder': '[后宫]立花オミナ'
-    }, {
       'artist': 'tachibana omina',
       'folder': '[后宫]立花オミナ'
     }, {
@@ -237,10 +237,10 @@ let config = {
       'folder': '[纯爱]神毛物由宇'
     }, {
       'artist': 'ninoko',
-      'folder': '[纯爱]ninoko'
+      'folder': '[纯爱型援助]ninoko'
     }, {
       'artist': 'akino sora',
-      'folder': 'akino sora'
+      'folder': '[妹]akino sora'
     }, {
       'artist': 'takei ooki',
       'folder': 'takei ooki'
@@ -257,7 +257,7 @@ let config = {
       'artist': 'ekakibit',
       'folder': '[萝莉]ekakibit'
     }, {
-      'artist': 'nekometaru | necometal',
+      'artist': 'nekometaru',
       'folder': '[萝莉]nekometaru'
     }, {
       'artist': 'bizen',
@@ -277,7 +277,7 @@ let config = {
       'artist': 'sugaishi',
       'folder': '[纯爱]sugaishi'
     }, {
-      'artist': 'usashiro mani | mani',
+      'artist': 'usashiro mani',
       'folder': '[萝莉]usashiro mani'
     }, {
       'artist': 'tamano kedama',
@@ -295,8 +295,11 @@ let config = {
       'artist': 'hitsujibane shinobu',
       'folder': 'hitsujibane shinobu'
     }, {
-      'artist': /endou okito/,
-      'folder': 'endou okito'
+      'artist': 'endou okito',
+      'folder': '[彩图]endou okito'
+    }, {
+      'artist': 'shiwasu no okina',
+      'folder': '[后宫]師走の翁'
     }, {
       'Uploader': 'HUILENDASI',
       'folder': '[后宫]鬼畜王汉化组'
@@ -318,7 +321,7 @@ let config = {
       'folder': '[标签]女性:NTR'
     }
   ],
-  'parody': [{
+  parody: [{
     name: 'Fate',
     filter: /^fate/gi
   }, {
@@ -352,7 +355,7 @@ let config = {
     name: '战场女武神',
     filter: /valkyria chronicles/gi
   }],
-  'removeCharacter': [
+  removeCharacter: [
     'teitoku', // 提督
     'producer', // 制作人
     'kazuto kirigaya' // 桐谷和人
--- comicSort/index.js
+++ comicSort/index.js
@@ -8,19 +8,22 @@
 // @Last Modified time: 2018-06-02 01:49:59
 // @Namespace:          https://github.com/dodying/Nodejs
 // @SupportURL:         https://github.com/dodying/Nodejs/issues
-// @Require:            readline-sync,async,jszip,superagent,image-size,tracer,colors,glob,mkdirp
+// @Require:            readline-sync,async,jszip,image-size,tracer,colors,glob,mkdirp,request,request-promise
 // ==/Headers==
 
 // 设置
 const _ = {
+  '7z': 'D:\\GreenSoftware\\_Basis\\7-Zip\\7z.exe', // 7z路径 (仅删除宣传图与压缩info.txt所需)
+  proxy: 'http://127.0.0.1:2346', // 代理
+  globRecursive: true, // 是否递归comicFolder
   comicFolder: 'F:\\Temp', // 需要整理的文件夹
   libraryFolder: 'F:\\ComicLibrary', // 整理到那个文件夹
   jTitle: false, // 是否重命名为日本名称
+  delIntroPic: true, // 是否删除宣传图
+  introPic: [],
   checkImageSize: true, // 是否检测图片大小
-  rateD: 1.1, // 双页图片宽高比
-  rateS: 0.9, // 单页图片宽高比
-  sizeD: 1280, // 双页的宽度
-  sizeS: 780, // 单页的宽度
+  rate: 1, // 图片宽高比的分界，大于则为双页
+  size: 780, // 小图的宽度
   cover: true, // 是否创建同名封面
   subFolder: [ // 子文件夹
     '0.Series',
@@ -46,28 +49,18 @@ const _ = {
     name: 'example',
     filter: 'example'
   }],
-  removeCharacter: ['teitoku', 'producer']
+  removeCharacter: [
+    'teitoku', // 提督
+    'producer' // 制作人
+  ]
 }
 
 // 导入原生模块
 const fs = require('fs')
-/* eslint-disable node/no-deprecated-api */
-fs.exists = path => {
-  try {
-    fs.statSync(path)
-  } catch (err) {
-    return false
-  }
-  return true
-}
 const path = require('path')
+const cp = require('child_process')
 
 // 导入第三方模块
-const readlineSync = require('readline-sync')
-const async = require('async')
-const JSZip = require('jszip')
-const superagent = require('superagent')
-const sizeOf = require('image-size')
 const logger = require('tracer').console({
   format: '{{timestamp}} <L{{line}}:{{pos}}>:\t{{message}}',
   dateformat: 'HH:MM:ss'
@@ -80,9 +73,21 @@ colors.setTheme({
   debug: 'blue',
   error: 'red'
 })
+const readlineSync = require('readline-sync')
+const async = require('async')
+const JSZip = require('jszip')
+const request = require('request-promise')
+const sizeOf = require('image-size')
 const glob = require('glob')
 const mkdirp = require('mkdirp')
 const emojireg = /\u{2139}|[\u{2194}-\u{2199}]|[\u{21A9}-\u{21AA}]|[\u{231A}-\u{231B}]|\u{2328}|\u{23CF}|[\u{23E9}-\u{23F3}]|[\u{23F8}-\u{23FA}]|\u{24C2}|[\u{25AA}-\u{25AB}]|\u{25B6}|\u{25C0}|[\u{25FB}-\u{25FE}]|[\u{2600}-\u{2604}]|\u{260E}|\u{2611}|[\u{2614}-\u{2615}]|\u{2618}|\u{261D}|\u{2620}|[\u{2622}-\u{2623}]|\u{2626}|\u{262A}|[\u{262E}-\u{262F}]|[\u{2638}-\u{263A}]|[\u{2648}-\u{2653}]|\u{2660}|\u{2663}|\u{2666}|\u{2668}|\u{267B}|\u{267F}|[\u{2692}-\u{2697}]|\u{2699}|[\u{269B}-\u{269C}]|[\u{26A0}-\u{26A1}]|[\u{26AA}-\u{26AB}]|[\u{26B0}-\u{26B1}]|[\u{26BD}-\u{26BE}]|[\u{26C4}-\u{26C5}]|\u{26C8}|\u{26CE}|\u{26CF}|\u{26D1}|[\u{26D3}-\u{26D4}]|[\u{26E9}-\u{26EA}]|[\u{26F0}-\u{26F5}]|[\u{26F7}-\u{26FA}]|\u{26FD}|\u{2702}|\u{2705}|[\u{2708}-\u{2709}]|[\u{270A}-\u{270B}]|[\u{270C}-\u{270D}]|\u{270F}|\u{2712}|\u{2714}|\u{2716}|\u{271D}|\u{2721}|\u{2728}|[\u{2733}-\u{2734}]|\u{2744}|\u{2747}|\u{274C}|\u{274E}|[\u{2753}-\u{2755}]|\u{2757}|\u{2763}|[\u{2795}-\u{2797}]|\u{27A1}|\u{27B0}|\u{27BF}|[\u{2934}-\u{2935}]|[\u{2B05}-\u{2B07}]|[\u{2B1B}-\u{2B1C}]|\u{2B50}|\u{2B55}|\u{3030}|\u{303D}|\u{3297}|\u{3299}|\u{1F004}|\u{1F0CF}|[\u{1F170}-\u{1F171}]|\u{1F17E}|\u{1F17F}|\u{1F18E}|[\u{1F191}-\u{1F19A}]|[\u{1F1E6}-\u{1F1FF}]|[\u{1F201}-\u{1F202}]|\u{1F21A}|\u{1F22F}|[\u{1F232}-\u{1F23A}]|[\u{1F250}-\u{1F251}]|[\u{1F300}-\u{1F320}]|\u{1F321}|[\u{1F324}-\u{1F32C}]|[\u{1F32D}-\u{1F32F}]|[\u{1F330}-\u{1F335}]|\u{1F336}|[\u{1F337}-\u{1F37C}]|\u{1F37D}|[\u{1F37E}-\u{1F37F}]|[\u{1F380}-\u{1F393}]|[\u{1F396}-\u{1F397}]|[\u{1F399}-\u{1F39B}]|[\u{1F39E}-\u{1F39F}]|[\u{1F3A0}-\u{1F3C4}]|\u{1F3C5}|[\u{1F3C6}-\u{1F3CA}]|[\u{1F3CB}-\u{1F3CE}]|[\u{1F3CF}-\u{1F3D3}]|[\u{1F3D4}-\u{1F3DF}]|[\u{1F3E0}-\u{1F3F0}]|[\u{1F3F3}-\u{1F3F5}]|\u{1F3F7}|[\u{1F3F8}-\u{1F3FF}]|[\u{1F400}-\u{1F43E}]|\u{1F43F}|\u{1F440}|\u{1F441}|[\u{1F442}-\u{1F4F7}]|\u{1F4F8}|[\u{1F4F9}-\u{1F4FC}]|\u{1F4FD}|\u{1F4FF}|[\u{1F500}-\u{1F53D}]|[\u{1F549}-\u{1F54A}]|[\u{1F54B}-\u{1F54E}]|[\u{1F550}-\u{1F567}]|[\u{1F56F}-\u{1F570}]|[\u{1F573}-\u{1F579}]|\u{1F57A}|\u{1F587}|[\u{1F58A}-\u{1F58D}]|\u{1F590}|[\u{1F595}-\u{1F596}]|\u{1F5A4}|\u{1F5A5}|\u{1F5A8}|[\u{1F5B1}-\u{1F5B2}]|\u{1F5BC}|[\u{1F5C2}-\u{1F5C4}]|[\u{1F5D1}-\u{1F5D3}]|[\u{1F5DC}-\u{1F5DE}]|\u{1F5E1}|\u{1F5E3}|\u{1F5E8}|\u{1F5EF}|\u{1F5F3}|\u{1F5FA}|[\u{1F5FB}-\u{1F5FF}]|\u{1F600}|[\u{1F601}-\u{1F610}]|\u{1F611}|[\u{1F612}-\u{1F614}]|\u{1F615}|\u{1F616}|\u{1F617}|\u{1F618}|\u{1F619}|\u{1F61A}|\u{1F61B}|[\u{1F61C}-\u{1F61E}]|\u{1F61F}|[\u{1F620}-\u{1F625}]|[\u{1F626}-\u{1F627}]|[\u{1F628}-\u{1F62B}]|\u{1F62C}|\u{1F62D}|[\u{1F62E}-\u{1F62F}]|[\u{1F630}-\u{1F633}]|\u{1F634}|[\u{1F635}-\u{1F640}]|[\u{1F641}-\u{1F642}]|[\u{1F643}-\u{1F644}]|[\u{1F645}-\u{1F64F}]|[\u{1F680}-\u{1F6C5}]|[\u{1F6CB}-\u{1F6CF}]|\u{1F6D0}|[\u{1F6D1}-\u{1F6D2}]|[\u{1F6E0}-\u{1F6E5}]|\u{1F6E9}|[\u{1F6EB}-\u{1F6EC}]|\u{1F6F0}|\u{1F6F3}|[\u{1F6F4}-\u{1F6F6}]|[\u{1F6F7}-\u{1F6F8}]|[\u{1F910}-\u{1F918}]|[\u{1F919}-\u{1F91E}]|\u{1F91F}|[\u{1F920}-\u{1F927}]|[\u{1F928}-\u{1F92F}]|\u{1F930}|[\u{1F931}-\u{1F932}]|[\u{1F933}-\u{1F93A}]|[\u{1F93C}-\u{1F93E}]|[\u{1F940}-\u{1F945}]|[\u{1F947}-\u{1F94B}]|\u{1F94C}|[\u{1F950}-\u{1F95E}]|[\u{1F95F}-\u{1F96B}]|[\u{1F980}-\u{1F984}]|[\u{1F985}-\u{1F991}]|[\u{1F992}-\u{1F997}]|\u{1F9C0}|[\u{1F9D0}-\u{1F9E6}]/gu
+const incestTags = {
+  grandmother: ['grandmother'],
+  mother: ['mother', 'aunt'],
+  sister: ['sister', 'cousin', 'brother'],
+  daughter: ['daughter', 'niece', 'father'],
+  granddaughter: ['granddaughter', 'grandfather']
+}
 
 const EHT = JSON.parse(fs.readFileSync('EHT.json', 'utf-8')).dataset
 
@@ -108,12 +113,13 @@ const unique = arr => [...(new Set(arr))]
 const escape = text => text.replace(/[\\/:*?"<>|]/g, '-').replace(/\.$/, '').replace(emojireg, '')
 const escape2 = text => text.replace(/[:*?"<>|]/g, '-').replace(/\.$/, '').replace(emojireg, '')
 const parseInfo = text => {
-  text = text.replace(/(Page|Image) \d+: .*/g, '').replace(/(Downloaded at|Generated by).*/g, '').replace(/([\r\n]){2,}/g, '\r\n').replace(/[\r\n]+$/g, '').replace(/\r\n> /g, '\r\n')
+  text = text.replace(/(Downloaded at|Generated by).*/g, '').replace(/([\r\n]){2,}/g, '\r\n').replace(/[\r\n]+$/g, '').replace(/\r\n> /g, '\r\n')
   let a = text.split(/\r\n/)
   let b = {}
   let info = {
     title: a[0],
-    jTitle: (a[1].match(/^http/)) ? a[0] : a[1]
+    jTitle: (a[1].match(/^http/)) ? a[0] : a[1],
+    page: []
   }
   let tags = ['language', 'reclass', 'artist', 'group', 'parody', 'character', 'female', 'male', 'misc']
   for (let i of a) {
@@ -122,12 +128,29 @@ const parseInfo = text => {
       break
     }
   }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i].match(/^Page \d+:/)) {
+      let re = a[i].match(/^Page (\d+):\s+(.*)$/)
+      info.page[re[1] * 1] = {
+        url: re[2],
+        id: re[2].split('/')[4]
+      }
+      a.splice(i, 1)
+      i--
+    } else if (a[i].match(/^Image \d+:/)) {
+      let re = a[i].match(/^Image (\d+):\s+(.*)$/)
+      info.page[re[1] * 1].name = re[2]
+      a.splice(i, 1)
+      i--
+    }
+  }
   for (let i of a) {
     let t = i.split(': ')
     if (t.length > 1) {
       b[t[0]] = tags.includes(t[0]) ? t[1].split(', ').sort() : t[1]
     }
   }
+
   Object.assign(info, b)
   if ('parody' in b) info.series = b.parody
   if (a.indexOf('Uploader Comment:') >= 0) info.summary = a.slice(a.indexOf('Uploader Comment:')).join('\r\n')
@@ -186,6 +209,17 @@ const sortFileBySpecialRule = info => {
         let _rule = rule[i][j]
         let _info = [].concat(info[j])
         if (typeof _rule === 'string') {
+          if (_info.some(k => k.match('|'))) {
+            let _info2 = []
+            _info.forEach(k => {
+              _info2 = _info2.concat(k.split('|'))
+            })
+            _info2 = _info2.map(k => k.trim())
+            if (_info2.includes(_rule)) {
+              filter = true
+              break
+            }
+          }
           if (_info.includes(_rule)) {
             filter = true
             break
@@ -213,6 +247,18 @@ const sortFileBySpecialRule = info => {
         let _rule = rule[i][j]
         let _info = [].concat(info[j])
         if (typeof _rule === 'string') {
+          if (_info.some(k => k.match('|'))) {
+            let _info2 = []
+            _info.forEach(k => {
+              _info2 = _info2.concat(k.split('|'))
+            })
+            _info2 = _info2.map(k => k.trim())
+            if (!_info2.includes(_rule)) {
+              filter = false
+              break
+            }
+            continue
+          }
           if (!_info.includes(_rule)) {
             filter = false
             break
@@ -270,7 +316,11 @@ const sortFile = info => {
   } else if ('female' in info && info.female.includes('harem')) {
     return _.subFolder[5]
   } else if (info.tags.includes('incest') || info.tags.includes('inseki')) {
-    return _.subFolder[6]
+    let tags = []
+    for (let i in incestTags) {
+      if (info.tags.some(tag => incestTags[i].includes(tag))) tags.push(i)
+    }
+    return _.subFolder[6] + (tags.length ? '/' + tags.sort().join(', ') : '')
   } else if (info.tags.includes('story arc')) {
     return _.subFolder[7]
   } else if ((info.tags.includes('anthology')) || (info.artist && info.artist.length > 2)) {
@@ -284,33 +334,11 @@ const sortFile = info => {
     return _.subFolder[10]
   }
 }
-const scrapeInfo = url => {
-  return new Promise((resolve, reject) => {
-    let pram = url.split('/')
-    superagent.post('https://e-hentai.org/api.php').send({
-      method: 'gdata',
-      gidlist: [[pram[4] * 1, pram[5]]],
-      namespace: 1
-    }).set('User-Agent', 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36').timeout(30 * 1000).end((err, res) => {
-      if (err) {
-        logger.error(`Request: ${colors.info(url)}\nInfo: `, err)
-        reject(err)
-      } else {
-        let json = JSON.parse(res.text)
-        if (!json.gmetadata) {
-          reject(new Error('Error post OR not find info'))
-          return
-        }
-        resolve(json)
-      }
-    })
-  })
-}
 const moveByInfo = (info, target) => {
   info.file = target
   let targetFolderNew = sortFile(info)
   targetFolderNew = path.resolve(_.libraryFolder, targetFolderNew)
-  if (!fs.exists(targetFolderNew)) mkdirp.sync(targetFolderNew)
+  if (!fs.existsSync(targetFolderNew)) mkdirp.sync(targetFolderNew)
   let nameNew = escape(_.jTitle ? info.jTitle : info.title)
   let targetNew = path.resolve(targetFolderNew, nameNew + '.cbz')
   if (targetNew === target) {
@@ -321,7 +349,7 @@ const moveByInfo = (info, target) => {
     let atime
     if (_.cover) {
       let targetCover = path.resolve(_.comicFolder, path.parse(target).name + '.jpg')
-      if (fs.exists(targetCover)) {
+      if (fs.existsSync(targetCover)) {
         atime = fs.statSync(targetCover).atime
         moveFile(targetCover, path.resolve(targetFolderNew, nameNew + '.jpg'))
       }
@@ -332,19 +360,21 @@ const moveByInfo = (info, target) => {
 }
 
 // Main
-if (fs.exists('config.js')) Object.assign(_, require('./config'))
+if (fs.existsSync('config.js')) Object.assign(_, require('./config'))
 
 const main = async () => {
   // 读取列表
-  let lst = glob.sync(_.comicFolder.replace(/\\/g, '/') + (_.globRecursive ? '/**' : '') + '/*.@(zip|cbz)')
+  let lst = glob.sync((_.globRecursive ? '**/' : '') + '*.@(zip|cbz)', {
+    cwd: _.comicFolder.replace(/\\/g, '/')
+  })
   if (lst.length) logger.log('当前任务数: ', colors.info(lst.length))
 
   // 开始处理
   let result = await mapSeriesSync(lst, async i => {
     // 处理路径
     let target = path.resolve(_.comicFolder, i)
-    let targetShort = path.relative(_.comicFolder, i)
-    logger.log(colors.info(targetShort))
+    let targetDir = path.parse(target).dir
+    logger.log(colors.info(i))
 
     // 读取数据
     let targetData = fs.readFileSync(target)
@@ -372,46 +402,50 @@ const main = async () => {
       return i
     }
 
-    // 检测图片及大小
-    if (_.checkImageSize) {
-      let imgs = fileList.filter(item => item.match(/\.(jpg|png|gif)$/) < 0 && !item.match(/\/$/))
-      let pageSingle = 0 // 单页
-      let pageDouble = 0 // 双叶
-      let pageSmall = 0 // 小页
-      let pageBig = 0 // 大页
-      let result1 = await mapSeriesSync(imgs, async j => {
-        try {
-          let img = await zip.files[j].async('nodebuffer')
-          let size = sizeOf(img)
-          if (size.width <= _.sizeS) {
-            pageSmall++
-          } else if (size.width >= _.sizeD) {
-            pageBig++
-          }
-          size = size.height / size.width
-          if (size > _.rateD) {
-            pageSingle++
-          } else if (size < _.rateS) {
-            pageDouble++
-          }
-        } catch (error) {
-          logger.error(error)
-        }
-        return j
-      })
-      if (result1[0]) {
-        logger.error(result1[0])
-        return
-      }
-      if ((pageSingle > pageDouble && pageSmall < imgs.length * 0.2) || (pageSingle < pageDouble && pageBig < imgs.length * 0.2)) {
-        logger.warn(colors.warn('图片大小错误，请重新下载: '), target)
-        return i
-      }
-    }
-
     // 读取info.txt
     let infoFile = fileList.find(item => item.match(/(^|\/)info\.txt$/))
     let data = await zip.files[infoFile].async('text')
+    let info = parseInfo(data)
+
+    // 检测图片及大小
+    if ((_.delIntroPic || _.checkImageSize) && info.web.match(/e(-|x)hentai.org/)) {
+      let imgs = fileList.filter(item => item.match(/\.(jpg|png|gif)$/))
+      for (let j = 0; j < imgs.length; j++) {
+        let deleted = false
+        if (_.delIntroPic) {
+          let name = path.parse(imgs[j]).base
+          let filter = info.page.filter(p => p.name === name)
+          if (filter.length && _.introPic.includes(filter[0].id)) {
+            logger.log(colors.error('Deleted: '), colors.info(imgs[j]))
+            cp.execSync(`${_['7z']} d -tzip -mx9 "${target}" "${imgs[j]}"`, {
+              cwd: targetDir
+            })
+            deleted = true
+          }
+        }
+        if (_.checkImageSize && !info.tags.includes('tankoubon') && !info.tags.includes('anthology') && !deleted) {
+          let img = await zip.files[imgs[j]].async('nodebuffer')
+          let size = sizeOf(img)
+          let rate = size.width / size.height
+          if (rate > _.rate && size.width === _.size) {
+            logger.log('Size:', colors.info(size.width, '*', size.height), '  Pages:', colors.info(Object.keys(info.page).length), '  Genre:', colors.info(info.genre))
+            logger.log('Page:', colors.warn(imgs[j]))
+            let web = info.web.replace('http:', 'https:').replace(/(g.|)e-hentai/, 'exhentai').replace(/#\d+$/, '') + '#2'
+            if (readlineSync.keyInYNStrict('Open ' + colors.info(web))) {
+              cp.execSync(`start "" "${web}"`)
+              if (readlineSync.keyInYNStrict('Delete ' + colors.info(target))) fs.unlinkSync(target)
+              return
+            } else {
+              if (readlineSync.keyInYNStrict('Sort this file: ' + colors.info(target))) {
+                break
+              } else {
+                return
+              }
+            }
+          }
+        }
+      }
+    }
 
     // 解压封面
     if (_.cover) {
@@ -430,51 +464,63 @@ const main = async () => {
       fs.utimesSync(targetCover, date, date)
     }
 
-    // 处理info.txt为json信息
-    let info = parseInfo(data)
-    if (!data.match(/Tags:/)) { // 如果不存在(EHD v1.23之前下载的)
-      try {
-        let json = await scrapeInfo(info.web.replace('http://g.e-hentai.org', 'https://e-hentai.org').replace('https://exhentai.org', 'https://e-hentai.org'))
-        let infoStr = 'Tags:\r\n'
-        let tagsList = json.gmetadata[0].tags
-        let tags = {}
-        tagsList.forEach(i => {
-          let a = i.split(':')
-          let key = a.length === 2 ? a[0] : 'misc'
-          if (!tags[key]) tags[key] = []
-          tags[key].push(a[1] || a[0])
+    // 如果info不存在tags(EHD v1.23之前下载的)
+    if (!data.match(/Tags:/) && info.web.match(/e(-|x)hentai.org/)) {
+      let url = info.web.replace(/^.*hentai.org/, 'https://e-hentai.org')
+      let pram = url.split('/')
+      let res = await request('https://e-hentai.org/api.php', {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36'
+        },
+        timeout: 30 * 1000,
+        resolveWithFullResponse: true,
+        proxy: _.proxy,
+        body: JSON.stringify({
+          method: 'gdata',
+          gidlist: [[pram[4] * 1, pram[5]]],
+          namespace: 1
         })
-        for (let i in tags) {
-          infoStr += `> ${i}: ${tags[i].join(', ')}\r\n`
-        }
-        data += '\r\n' + infoStr
-        let buffer = await zip.file(infoFile, data).generateAsync({
-          type: 'nodebuffer',
-          compression: 'DEFLATE',
-          compressionOptions: {
-            level: 9
-          }
-        })
-        try {
-          fs.writeFileSync(target, buffer)
-        } catch (err) {
-          logger.log(colors.warn(data))
-          logger.error('写入压缩错误: ', colors.error(target))
-          readlineSync.keyInPause(colors.warn('你可以尝试手动将以上信息写入info.txt'))
-        }
-        moveByInfo(info, target)
-        return i
-      } catch (error) {
-        logger.warn(colors.warn('请求信息失败: ', target, ' ', info.web))
-        return i
+      })
+      let json = JSON.parse(res.body)
+      let infoStr = '\r\nTags:\r\n'
+      let tagsList = json.gmetadata[0].tags
+      let tags = {}
+      tagsList.forEach(i => {
+        let a = i.split(':')
+        let key = a.length === 2 ? a[0] : 'misc'
+        if (!tags[key]) tags[key] = []
+        tags[key].push(a[1] || a[0])
+      })
+      for (let i in tags) {
+        infoStr += `> ${i}: ${tags[i].join(', ')}\r\n`
       }
-    } else {
-      moveByInfo(info, target)
+      data += '\r\n' + infoStr
+
+      let infoFileDir = path.resolve(targetDir, path.parse(infoFile).dir)
+      mkdirp.sync(infoFileDir)
+      let infoFilePath = path.resolve(infoFileDir, 'info.txt')
+      fs.writeFileSync(infoFilePath, data)
+
+      cp.execSync(`${_['7z']} a -tzip -mx9 "${target}" "${infoFile}"`, {
+        cwd: targetDir
+      })
+
+      fs.unlinkSync(infoFilePath)
+      fs.rmdirSync(infoFileDir)
       return i
     }
+
+    // 整理
+    moveByInfo(info, target)
+    return i
   })
   if (result[0]) logger.error(result[0])
   if (lst.length) logger.log(colors.info('任务完成'))
 }
 
-main()
+main().then(() => {
+  //
+}, (err) => {
+  logger.error(err)
+})
