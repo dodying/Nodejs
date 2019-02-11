f1f3490282bf441a7c860e273ef6348d3c927d21
dodying <549779963@qq.com>
2019-02-11 22:23:55 +0800

    comicDelete v1.0.2

--- comicDelete/index.js
+++ comicDelete/index.js
@@ -2,23 +2,24 @@
 
 // ==Headers==
 // @Name:               comicDelete
-// @Description:        comicDelete
-// @Version:            1.0.0
+// @Description:        删除漫画
+// @Version:            1.0.2
 // @Author:             dodying
-// @Date:               2018-06-18 15:47:59
-// @Last Modified by:   dodying
-// @Last Modified time: 2018-06-18 15:47:59
+// @Date:               2019-2-10 10:59:27
 // @Namespace:          https://github.com/dodying/Nodejs
 // @SupportURL:         https://github.com/dodying/Nodejs/issues
 // @Require:            readline-sync
 // ==/Headers==
 
 // 设置
-const deletedPath = 'F:\\ComicLibrary\\X.Deleted'
+let deletedPath = 'F:\\H\\###ComicLibrary\\X.Deleted'
+const extensions = ['.cbz', '.jpg', '.zip', '.png']
 
 // 导入原生模块
 const fs = require('fs')
 const path = require('path')
+let deletedPathFolderName = path.parse(path.parse(deletedPath).dir).base
+let deletedPathName = path.parse(deletedPath).base
 
 // 导入第三方模块
 const readlineSync = require('readline-sync')
@@ -27,13 +28,33 @@ const readlineSync = require('readline-sync')
 
 // Main
 
-process['argv'].splice(2).forEach(i => {
+process['argv'].splice(2).filter(i => extensions.includes(path.parse(i).ext)).forEach(i => {
   let p = path.resolve(process.cwd(), i)
   if (fs.existsSync(p)) {
+    if (!fs.existsSync(deletedPath)) {
+      if (p.match('ComicLibrary')) {
+        let deletedPathFolder = p
+        while (path.parse(deletedPathFolder).base !== deletedPathFolderName) {
+          deletedPathFolder = path.parse(deletedPathFolder).dir
+        }
+        deletedPath = path.join(deletedPathFolder, deletedPathName)
+      } else {
+        console.error('NOT find deletedPath:\t', deletedPath)
+      }
+    }
     console.log(p)
-    if (p.match('F:\\\\ComicLibrary') || readlineSync.keyInYNStrict('Continue to delete?')) {
+    if (p.match('ComicLibrary') || readlineSync.keyInYNStrict('Continue to delete?')) {
       fs.unlinkSync(p)
-      fs.writeFileSync(path.resolve(deletedPath, path.parse(p).base), '')
+
+      let { dir, name } = path.parse(p)
+      let cover = path.join(dir, name + '.jpg')
+      if (fs.existsSync(cover)) fs.unlinkSync(cover)
+
+      try {
+        fs.writeFileSync(path.resolve(deletedPath, path.parse(p).base), '')
+      } catch (error) {
+        console.error(error)
+      }
       console.log('File Deleted:\t', p)
     }
   } else {
