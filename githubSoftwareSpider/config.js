8984f8c134edae46e748331b79d2e2810da9a3da
dodying <549779963@qq.com>
2020-06-02 13:04:54 +0800

    use semi-standard instead of standard

--- githubSoftwareSpider/config.js
+++ githubSoftwareSpider/config.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
-let data = {
+const data = {
   request: {
     userAgent: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
     retry: 5,
     timeout: 30,
     proxy: 'http://127.0.0.1:2345'
   }
-}
-module.exports = data
+};
+module.exports = data;
--- githubSoftwareSpider/index.js
+++ githubSoftwareSpider/index.js
@@ -11,17 +11,17 @@
 // ==/Headers==
 
 // 导入原生模块
-const fs = require('fs')
+const fs = require('fs');
 // const url = require('fs')
 // const path = require('path')
 
 // 导入第三方模块
-const requestPromise = require('request-promise')
-const Agent = require('socks5-http-client/lib/Agent')
-const Agent2 = require('socks5-https-client/lib/Agent')
-const cheerio = require('cheerio')
-const iconv = require('iconv-lite')
-var ON_DEATH = require('death')
+const requestPromise = require('request-promise');
+const Agent = require('socks5-http-client/lib/Agent');
+const Agent2 = require('socks5-https-client/lib/Agent');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+var ON_DEATH = require('death');
 
 // 设置
 const config = {
@@ -29,131 +29,131 @@ const config = {
   uriLast: null,
   cookies: requestPromise.jar(),
   retryList: {}
-}
+};
 
 // Function
 function reqOption (uriOrOption, optionUser = {}) {
-  let option = typeof uriOrOption === 'string' ? { uri: uriOrOption } : uriOrOption
-  let uri = option.uri
-  let useProxy = optionUser.useProxy
+  let option = typeof uriOrOption === 'string' ? { uri: uriOrOption } : uriOrOption;
+  let uri = option.uri;
+  const useProxy = optionUser.useProxy;
 
-  if (config.uriLast) uri = new URL(uri, config.uriLast).href
+  if (config.uriLast) uri = new URL(uri, config.uriLast).href;
   option = Object.assign({
     method: 'GET',
     headers: {
       'User-Agent': config._.request.userAgent,
-      'Accept': 'application/json, text/plain, */*',
+      Accept: 'application/json, text/plain, */*',
       // 'Accept-Language': '*;q=0.5',
-      'Referer': config.uriLast && new URL(config.uriLast).hostname === new URL(uri).hostname ? config.uriLast : uri,
-      'Authorization': 'token 28c1e498757d7c21e37306bb89df4a093f391083'
+      Referer: config.uriLast && new URL(config.uriLast).hostname === new URL(uri).hostname ? config.uriLast : uri,
+      Authorization: 'token 28c1e498757d7c21e37306bb89df4a093f391083'
     },
     timeout: config._.request.timeout * 1000,
     jar: config.cookies,
     strictSSL: false,
     resolveWithFullResponse: true,
     simple: false
-  }, option, { uri })
+  }, option, { uri });
 
-  uri = option.uri || option.url
-  delete option.url
+  uri = option.uri || option.url;
+  delete option.url;
 
   if (useProxy && config._.request.proxy && config._.request.proxy.match(/(http|socks5):\/\/((.*?):(.*?)@)?(.*?):(\d+)/i)) {
-    let [, protocol,, username, password, hostname, port] = config._.request.proxy.match(/(http|socks5):\/\/((.*?):(.*?)@)?(.*?):(\d+)/i)
+    const [, protocol,, username, password, hostname, port] = config._.request.proxy.match(/(http|socks5):\/\/((.*?):(.*?)@)?(.*?):(\d+)/i);
     if (protocol.toLowerCase() === 'http') {
-      option.proxy = config._.request.proxy
+      option.proxy = config._.request.proxy;
     } else if (protocol.toLowerCase() === 'socks5') {
-      option.agentClass = uri.match(/^http:/) ? Agent : Agent2
+      option.agentClass = uri.match(/^http:/) ? Agent : Agent2;
       option.agentOptions = {
         socksHost: hostname,
         socksPort: port
-      }
+      };
       if (username && password) {
-        option.agentOptions.socksUsername = username
-        option.agentOptions.socksPassword = password
+        option.agentOptions.socksUsername = username;
+        option.agentOptions.socksPassword = password;
       }
     }
   }
-  console.warn(`${option.method}${option.proxy ? '+proxy' : ''}:\t${uri}`)
+  console.warn(`${option.method}${option.proxy ? '+proxy' : ''}:\t${uri}`);
 
-  config.uriLast = uri
-  return option
+  config.uriLast = uri;
+  return option;
 }
 
 async function req (uriOrOption, optionUser = {}) {
-  let option = reqOption(uriOrOption, optionUser)
-  let uri = option.uri || option.url
+  const option = reqOption(uriOrOption, optionUser);
+  const uri = option.uri || option.url;
 
-  let errorHandle = (message) => {
-    config.retryList[uri] = uri in config.retryList ? config.retryList[uri] + 1 : 1
+  const errorHandle = (message) => {
+    config.retryList[uri] = uri in config.retryList ? config.retryList[uri] + 1 : 1;
 
-    console.error(`Try-${config.retryList[uri]}:\t${message}`)
-    return config._.request.retry > config.retryList[uri] ? req(uriOrOption, optionUser) : null
-  }
+    console.error(`Try-${config.retryList[uri]}:\t${message}`);
+    return config._.request.retry > config.retryList[uri] ? req(uriOrOption, optionUser) : null;
+  };
 
-  let res
+  let res;
   try {
-    if (!('encoding' in option)) option.encoding = null
-    res = await requestPromise(option)
+    if (!('encoding' in option)) option.encoding = null;
+    res = await requestPromise(option);
     if (option.encoding === null) {
       // 判断网页编码
-      let charset = res.headers['content-type'] && res.headers['content-type'].match(/charset=(.*?)(;|$)/) ? res.headers['content-type'].match(/charset=(.*?)(;|$)/i)[1] : 'utf-8'
+      let charset = res.headers['content-type'] && res.headers['content-type'].match(/charset=(.*?)(;|$)/) ? res.headers['content-type'].match(/charset=(.*?)(;|$)/i)[1] : 'utf-8';
 
-      let body
+      let body;
       try {
-        body = iconv.decode(res.body, charset)
+        body = iconv.decode(res.body, charset);
       } catch (error) {
-        body = iconv.decode(res.body, 'utf-8')
+        body = iconv.decode(res.body, 'utf-8');
       }
 
-      let $ = cheerio.load(body)
+      const $ = cheerio.load(body);
       if ($('meta[http-equiv="Content-Type"][content*="charset"]').length || $('meta[charset]').length) {
         if ($('meta[http-equiv="Content-Type"][content*="charset"]').length) {
-          charset = $('meta[http-equiv="Content-Type"][content*="charset"]').attr('content').match(/charset=(.*?)(;|$)/)[1]
+          charset = $('meta[http-equiv="Content-Type"][content*="charset"]').attr('content').match(/charset=(.*?)(;|$)/)[1];
         } else if ($('meta[charset]').length) {
-          charset = $('meta[charset]').attr('charset')
+          charset = $('meta[charset]').attr('charset');
         }
         try {
-          body = iconv.decode(res.body, charset)
+          body = iconv.decode(res.body, charset);
         } catch (error) {
-          body = iconv.decode(res.body, 'utf-8')
+          body = iconv.decode(res.body, 'utf-8');
         }
       }
-      res.body = body
+      res.body = body;
     }
-    let succeed = typeof optionUser.check === 'function' ? optionUser.check(res) : res.statusCode >= 200 || res.statusCode < 300
+    const succeed = typeof optionUser.check === 'function' ? optionUser.check(res) : res.statusCode >= 200 || res.statusCode < 300;
     if (succeed) {
       if (res.body) {
         try {
-          res.json = JSON.parse(res.body)
+          res.json = JSON.parse(res.body);
         } catch (error) {}
       }
-      delete config.retryList[uri]
-      return res
+      delete config.retryList[uri];
+      return res;
     } else {
-      return errorHandle(res.statusCode + ' ' + res.statusMessage)
+      return errorHandle(res.statusCode + ' ' + res.statusMessage);
     }
   } catch (error) {
     if (error.cause && error.cause.errno === 'ETIMEDOUT' && error.cause.port === 443 && uri.match('http://')) {
-      option.uri = uri.replace('http://', 'https://')
-      return req(option, optionUser)
+      option.uri = uri.replace('http://', 'https://');
+      return req(option, optionUser);
     } else {
-      return errorHandle(error.message)
+      return errorHandle(error.message);
     }
   }
 }
 
 // Main
-let database
+let database;
 const main = async () => {
   if (fs.existsSync('./database.json')) {
-    database = fs.readFileSync('database.json', 'utf-8')
-    database = JSON.parse(database)
-    let lastUrl = [].concat(...Object.values(database.wait))
+    database = fs.readFileSync('database.json', 'utf-8');
+    database = JSON.parse(database);
+    let lastUrl = [].concat(...Object.values(database.wait));
     if (lastUrl.length === 0) {
-      console.error(`None`)
-      process.exit(-1)
+      console.error('None');
+      process.exit(-1);
     } else {
-      lastUrl = lastUrl[0]
+      lastUrl = lastUrl[0];
     }
   } else {
     database = {
@@ -168,63 +168,63 @@ const main = async () => {
         star: []
       },
       repo: []
-    }
+    };
   }
   ON_DEATH(function (signal, err) {
-    fs.writeFileSync('./database.json', JSON.stringify(database, null, 2))
-    process.exit()
-  })
+    fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
+    process.exit();
+  });
 
   while ([].concat(...Object.values(database.wait)).length) {
     if (database.wait.repo.length) {
-      let repo = database.wait.repo[0]
+      const repo = database.wait.repo[0];
       if (!database.done.repo.includes(repo)) {
-        let res = await req(`https://api.github.com/repos/${repo}/releases`)
+        const res = await req(`https://api.github.com/repos/${repo}/releases`);
         if (!(res.json instanceof Array)) {
-          console.log(res.json)
+          console.log(res.json);
           if (res.json.message.match(/^API rate limit exceeded for/)) {
-            break
+            break;
           }
         } else if (res.json.filter(i => i.assets.length).length) {
-          database.repo.push(`https://github.com/${repo}/releases`)
+          database.repo.push(`https://github.com/${repo}/releases`);
         }
 
-        database.done.repo.push(repo)
+        database.done.repo.push(repo);
       }
-      database.wait.repo.splice(0, 1)
+      database.wait.repo.splice(0, 1);
     } else if (database.wait.user.length) {
-      let user = database.wait.user[0]
+      const user = database.wait.user[0];
       if (!database.done.user.includes(user)) {
-        let res = await req(`https://api.github.com/users/${user}/repos`)
-        let repos = res.json.map(i => i.full_name)
-        database.wait.repo.push(...repos)
+        const res = await req(`https://api.github.com/users/${user}/repos`);
+        const repos = res.json.map(i => i.full_name);
+        database.wait.repo.push(...repos);
 
-        database.done.user.push(user)
+        database.done.user.push(user);
       }
-      database.wait.user.splice(0, 1)
+      database.wait.user.splice(0, 1);
     } else if (database.wait.star.length) {
-      let user = database.wait.star[0]
+      const user = database.wait.star[0];
       if (!database.done.star.includes(user)) {
-        let res = await req(`https://api.github.com/users/${user}/starred`)
-        let repos = res.json.map(i => i.full_name)
-        database.wait.repo.push(...repos)
-        let users = res.json.map(i => i.owner.login)
-        database.wait.user.push(...users)
-        database.wait.star.push(...users)
+        const res = await req(`https://api.github.com/users/${user}/starred`);
+        const repos = res.json.map(i => i.full_name);
+        database.wait.repo.push(...repos);
+        const users = res.json.map(i => i.owner.login);
+        database.wait.user.push(...users);
+        database.wait.star.push(...users);
 
-        database.done.star.push(user)
+        database.done.star.push(user);
       }
-      database.wait.star.splice(0, 1)
+      database.wait.star.splice(0, 1);
     }
   }
-}
+};
 
 main().then(async () => {
-  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2))
+  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
   //
 }, async err => {
-  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2))
+  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
 
-  console.error(err)
-  process.exit()
-})
+  console.error(err);
+  process.exit();
+});
