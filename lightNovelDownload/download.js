dd0a98860cf12cfabc5ce7906ea3690c0aa57697
dodying <549779963@qq.com>
2020-12-13 13:31:43 +0800

    v1.0.228

---
+++ lightNovelDownload/download.js
@@ -0,0 +1,83 @@
+// ==Headers==
+// @Name:               download
+// @Description:        download
+// @Version:            1.0.167
+// @Author:             dodying
+// @Date:               2019-2-12 18:41:49
+// @Namespace:          https://github.com/dodying/Nodejs
+// @SupportURL:         https://github.com/dodying/Nodejs/issues
+// @Require:            fs-extra
+// ==/Headers==
+
+// 导入原生模块
+
+// 导入第三方模块
+const fse = require('fs-extra')
+
+// Main
+const main = async () => {
+  let data = fse.existsSync('data.json') ? fse.readJSONSync('data.json') : { items: [], files: [], manual: [] }
+  data.manual = []
+  let gdriveDir = []
+  let gdrive = []
+  let mega = []
+  if (!fse.existsSync('file')) fse.mkdirSync('file')
+
+  for (let i = 0; i < data.files.length; i++) {
+    data.files[i].href = [].concat(data.files[i].href).filter((item, index, array) => item && !item.match(/blogspot.com|blogger/) && array.indexOf(item) === index)
+    if (data.files[i].href.length === 1) data.files[i].href = data.files[i].href[0]
+  }
+  data.files.sort((a, b) => a.title > b.title ? 1 : a.title === b.title ? 0 : -1)
+
+  for (let i of data.files) {
+    let href = [].concat(i.href)
+    if (href.filter(i => i.match('://mega')).length) {
+      href = href.filter(i => i.match('://mega'))[0]
+      mega.push(href)
+    } else if (href.filter(i => i.match(/(docs|drive).google.com/)).length) {
+      href = href.filter(i => i.match(/(docs|drive).google.com/))[0]
+      let id
+      if (href.match(/folders\/(.*)(\?|$)/) || href.match(/folderview\?.*id=(.*)/)) {
+        id = (href.match(/folders\/(.*)(\?|$)/) || href.match(/folderview\?.*id=(.*)/))[1]
+        gdriveDir.push(id)
+        continue
+      } else if (href.match(/file\/d\/(.*)\//)) {
+        id = href.match(/file\/d\/(.*)\//)[1]
+      } else if (href.match(/id=(.*)($|&)/)) {
+        id = href.match(/id=(.*)($|&)/)[1]
+      } else {
+        console.log(href)
+        data.manual.push(i)
+        continue
+      }
+
+      gdrive.push(`https://docs.google.com/uc?id=${id}&export=download`)
+    } else {
+      data.manual.push(i)
+    }
+  }
+  fse.writeJSONSync('data.json', data, { spaces: 2 })
+
+  let gdriveDirAll = fse.existsSync('all-gdriveDir.txt') ? fse.readFileSync('all-gdriveDir.txt', 'utf-8').split(/\r\n/) : []
+  gdriveDir = gdriveDir.filter(i => !gdriveDirAll.includes(i))
+  fse.appendFileSync('now-gdriveDir.txt', gdriveDir.join('\r\n'))
+  fse.appendFileSync('now-gdriveDir.bat', gdriveDir.map(i => ['gdrive.exe', 'download', '--path', 'file', '--force', '--recursive', i].join(' ')).join('\r\n'))
+  fse.appendFileSync('all-gdriveDir.txt', gdriveDir.join('\r\n'))
+
+  let gdriveAll = fse.existsSync('all-gdrive.txt') ? fse.readFileSync('all-gdrive.txt', 'utf-8').split(/\r\n/) : []
+  gdrive = gdrive.filter(i => !gdriveAll.includes(i))
+  fse.appendFileSync('now-gdrive.txt', gdrive.join('\r\n'))
+  fse.appendFileSync('all-gdrive.txt', gdrive.join('\r\n'))
+
+  let megaAll = fse.existsSync('all-mega.txt') ? fse.readFileSync('all-mega.txt', 'utf-8').split(/\r\n/) : []
+  mega = mega.filter(i => !megaAll.includes(i))
+  fse.appendFileSync('now-mega.txt', mega.join('\r\n'))
+  fse.appendFileSync('all-mega.txt', mega.join('\r\n'))
+}
+
+main().then(async () => {
+  //
+}, async err => {
+  console.error(err)
+  process.exit()
+})
---
+++ lightNovelDownload/index.js
@@ -0,0 +1,103 @@
+// ==Headers==
+// @Name:               ligntNovelDownload
+// @Description:        轻小说下载器，[来源](https://epubln.blogspot.com)
+// @Version:            1.0.228
+// @Author:             dodying
+// @Date:               2019-2-12 15:52:44
+// @Namespace:          https://github.com/dodying/Nodejs
+// @SupportURL:         https://github.com/dodying/Nodejs/issues
+// @Require:            cheerio,fs-extra
+// ==/Headers==
+
+// 导入原生模块
+// const path = require('path')
+// const url = require('url')
+
+// 导入第三方模块
+const fse = require('fs-extra');
+// const cheerio = require('cheerio');
+const req = require('./../_lib/req');
+req.config.init({
+  proxy: 'socks5://127.0.0.1:1088',
+  request: {
+    timeout: 60 * 1000,
+    followAllRedirects: false,
+    strictSSL: true
+  },
+  autoProxy: true,
+  withProxy: ['blogspot.com'],
+  logLevel: ['warn', 'error']
+});
+require('./../_lib/log').hack();
+
+// Main
+const database = fse.existsSync('./database.json') ? fse.readJSONSync('./database.json') : { items: [], files: [], manual: [] };
+const doExit = (exit = true) => {
+  if (typeof database === 'object' && database instanceof Object) {
+    fse.writeFileSync('./database.json', JSON.stringify(database, (key, value) => {
+      if (typeof value === 'object' && value instanceof Array) {
+        return Array.from(new Set(value));
+      }
+      return value;
+    }, 2));
+  }
+  if (exit) process.exit();
+};
+const main = async () => {
+  let link = 'https://epubln.blogspot.com';
+  // let link = 'https://epubln.blogspot.com/search?max-results=50';
+  // let link = 'https://epubln.blogspot.com/search/?q=a';
+  // let link = 'https://epubln.blogspot.com/2020/03/';
+
+  setInterval(() => {
+    doExit(false);
+  }, 1000 * 60 * 10);
+
+  while (link) {
+    console.log(`Index:\t${link}`);
+    const res = await req(link);
+    const $ = res.$;
+    const items = $('[rel=\'bookmark\']').map((index, item) => {
+      return {
+        href: item.attribs.href,
+        title: item.attribs.title.replace('Permanent Link to', '').replace('Permalink to', '').trim()
+      };
+    }).toArray();
+    for (const i of items) {
+      if (database.items.filter(j => j === i.href).length === 0) {
+        console.log(`Page:\t${i.href}`);
+        const res1 = await req(i.href);
+        const $1 = res1.$;
+        let links = $1('.cover a[href]').map((index, item) => item.attribs.href).toArray().filter((item, index, array) => item && !item.match(/blogspot.com|blogger/) && array.indexOf(item) === index);
+        if (links.length === 1) links = links[0];
+        database.files.push({
+          href: links,
+          refer: i.href,
+          title: i.title
+        });
+        database.items.push(i.href);
+      }
+    }
+
+    link = $('#Blog1_blog-pager-older-link').eq(0).attr('href');
+  }
+};
+
+try {
+  process.once('SIGINT', function () {
+    doExit(false);
+    console.log('SIGINT');
+    process.exit();
+  });
+
+  main().then(async () => {
+    doExit();
+  }, async err => {
+    console.error(err);
+
+    doExit();
+  });
+} catch (error) {
+  console.error(error);
+  doExit();
+}
