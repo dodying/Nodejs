313e39ad3a4001ae61c331b746cdd04b9d1bbb5e
dodying <549779963@qq.com>
2020-12-13 13:34:34 +0800

    v1.0.472

--- githubSoftwareSpider/config.js
+++ githubSoftwareSpider/config.js
@@ -1,11 +1,63 @@
 'use strict';
 
 const data = {
-  request: {
-    userAgent: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-    retry: 5,
-    timeout: 30,
-    proxy: 'http://127.0.0.1:2345'
+  req: {
+    proxy: 'http://127.0.0.1:8118',
+    request: {
+      headers: {
+        Authorization: 'token xxx'
+      },
+      timeout: 60 * 1000
+    },
+    withProxy: [
+      // 'github.com'
+      // '/google/'
+    ],
+    autoProxy: true
+  },
+  thread: 10,
+  repoFilter: (i) => {
+    const hot = i.subscribers_count > 5 || i.stargazers_count > 20 || i.forks_count > 10;
+    const hasDownloads = i.has_downloads;
+    const isContinue = new Date().getTime() - new Date(i.updated_at).getTime() <= 1000 * 60 * 60 * 24 * 365 * (i.archived ? 3 : 5);
+    const isFork = i.fork;
+    const isUnlikeLanguage = [
+      null,
+      'HTML', 'CSS', 'PHP', 'Vue',
+      'Swift', 'Objective-C', 'Objective-C++'
+    ].includes(i.language);
+    const descriptionExclude = [
+      /([^A-Z]|\b)(Library|Plugin|framework|module|SDK|Software Development Kit)([^A-Z]|\b)/i,
+      /([^A-Z]|\b)(macOS)([^A-Z]|\b)/i
+    ].some(re => !i.description || i.description.match(re));
+    return hot && hasDownloads && isContinue && !isFork && !isUnlikeLanguage && !descriptionExclude;
+  },
+  releaseFilter: (item, index) => {
+    if (index > 5) return false;
+    const { extname } = require('path');
+    // const extname = (file) => '.' + file.split(/[\\/.]+/).slice(-1)[0];
+
+    const hasAssets = item.assets.some(j => {
+      const filter = [
+        /([^A-Z]|\b)(win(dows)?|vc|mingw(64)?|cygwin|exe)([^A-Z]|\b)/i
+      ].some(re => j.name.match(re));
+      const sizeLarge = j.size > 1024 * 1024;
+      const sizeSmall = j.size > 1024 * 10;
+
+      const ext = ['.zip', '.7z'].includes(extname(j.name));
+      const ext1 = ['.exe', '.msi', '.rar', '.jar', '.appx', '.appxbundle', '.msix', '.msixbundle'].includes(extname(j.name));
+      const ext2 = ['.tar', '.gz', '.bz2', '.xz', '.nupkg'].includes(extname(j.name));
+
+      const exclude = [
+        /([^A-Z]|\b)(mac(os)?|app|osx|darwin|deb|ubuntu|linux|framework|demos?)([^A-Z]|\b)/i
+      ].some(re => j.name.match(re));
+      const downloadCount = j.download_count > 10;
+
+      return !exclude && downloadCount && ((ext && sizeLarge) || ((ext || ext2) && filter && sizeSmall) || (ext1 && sizeSmall));
+    });
+    const lastUpdate = new Date().getTime() - new Date(item.published_at).getTime() <= 1000 * 60 * 60 * 24 * 365 * 5;
+
+    return hasAssets && lastUpdate;
   }
 };
 module.exports = data;
--- githubSoftwareSpider/index.js
+++ githubSoftwareSpider/index.js
@@ -1,160 +1,51 @@
 // ==Headers==
 // @Name:               githubSoftwareSpider
 // @Description:        githubSoftwareSpider
-// @Version:            1.0.194
+// @Version:            1.0.472
 // @Author:             dodying
 // @Created:            2019-12-12 18:17:57
-// @Modified:           2019-12-12 20:35:08
+// @Modified:           2020/6/10 14:52:17
 // @Namespace:          https://github.com/dodying/Nodejs
 // @SupportURL:         https://github.com/dodying/Nodejs/issues
-// @Require:            cheerio,death,iconv-lite,request-promise,socks5-http-client,socks5-https-client
+// @Require:            null
 // ==/Headers==
+
+const config = require('./config');
 
 // 导入原生模块
 const fs = require('fs');
 // const url = require('fs')
-// const path = require('path')
+// const path = require('path');
 
 // 导入第三方模块
-const requestPromise = require('request-promise');
-const Agent = require('socks5-http-client/lib/Agent');
-const Agent2 = require('socks5-https-client/lib/Agent');
-const cheerio = require('cheerio');
-const iconv = require('iconv-lite');
-var ON_DEATH = require('death');
-
-// 设置
-const config = {
-  _: require('./config'),
-  uriLast: null,
-  cookies: requestPromise.jar(),
-  retryList: {}
-};
-
-// Function
-function reqOption (uriOrOption, optionUser = {}) {
-  let option = typeof uriOrOption === 'string' ? { uri: uriOrOption } : uriOrOption;
-  let uri = option.uri;
-  const useProxy = optionUser.useProxy;
-
-  if (config.uriLast) uri = new URL(uri, config.uriLast).href;
-  option = Object.assign({
-    method: 'GET',
-    headers: {
-      'User-Agent': config._.request.userAgent,
-      Accept: 'application/json, text/plain, */*',
-      // 'Accept-Language': '*;q=0.5',
-      Referer: config.uriLast && new URL(config.uriLast).hostname === new URL(uri).hostname ? config.uriLast : uri,
-      Authorization: 'token 28c1e498757d7c21e37306bb89df4a093f391083'
-    },
-    timeout: config._.request.timeout * 1000,
-    jar: config.cookies,
-    strictSSL: false,
-    resolveWithFullResponse: true,
-    simple: false
-  }, option, { uri });
-
-  uri = option.uri || option.url;
-  delete option.url;
-
-  if (useProxy && config._.request.proxy && config._.request.proxy.match(/(http|socks5):\/\/((.*?):(.*?)@)?(.*?):(\d+)/i)) {
-    const [, protocol,, username, password, hostname, port] = config._.request.proxy.match(/(http|socks5):\/\/((.*?):(.*?)@)?(.*?):(\d+)/i);
-    if (protocol.toLowerCase() === 'http') {
-      option.proxy = config._.request.proxy;
-    } else if (protocol.toLowerCase() === 'socks5') {
-      option.agentClass = uri.match(/^http:/) ? Agent : Agent2;
-      option.agentOptions = {
-        socksHost: hostname,
-        socksPort: port
-      };
-      if (username && password) {
-        option.agentOptions.socksUsername = username;
-        option.agentOptions.socksPassword = password;
-      }
-    }
-  }
-  console.warn(`${option.method}${option.proxy ? '+proxy' : ''}:\t${uri}`);
-
-  config.uriLast = uri;
-  return option;
-}
-
-async function req (uriOrOption, optionUser = {}) {
-  const option = reqOption(uriOrOption, optionUser);
-  const uri = option.uri || option.url;
-
-  const errorHandle = (message) => {
-    config.retryList[uri] = uri in config.retryList ? config.retryList[uri] + 1 : 1;
-
-    console.error(`Try-${config.retryList[uri]}:\t${message}`);
-    return config._.request.retry > config.retryList[uri] ? req(uriOrOption, optionUser) : null;
-  };
-
-  let res;
-  try {
-    if (!('encoding' in option)) option.encoding = null;
-    res = await requestPromise(option);
-    if (option.encoding === null) {
-      // 判断网页编码
-      let charset = res.headers['content-type'] && res.headers['content-type'].match(/charset=(.*?)(;|$)/) ? res.headers['content-type'].match(/charset=(.*?)(;|$)/i)[1] : 'utf-8';
-
-      let body;
-      try {
-        body = iconv.decode(res.body, charset);
-      } catch (error) {
-        body = iconv.decode(res.body, 'utf-8');
-      }
-
-      const $ = cheerio.load(body);
-      if ($('meta[http-equiv="Content-Type"][content*="charset"]').length || $('meta[charset]').length) {
-        if ($('meta[http-equiv="Content-Type"][content*="charset"]').length) {
-          charset = $('meta[http-equiv="Content-Type"][content*="charset"]').attr('content').match(/charset=(.*?)(;|$)/)[1];
-        } else if ($('meta[charset]').length) {
-          charset = $('meta[charset]').attr('charset');
-        }
-        try {
-          body = iconv.decode(res.body, charset);
-        } catch (error) {
-          body = iconv.decode(res.body, 'utf-8');
-        }
-      }
-      res.body = body;
-    }
-    const succeed = typeof optionUser.check === 'function' ? optionUser.check(res) : res.statusCode >= 200 || res.statusCode < 300;
-    if (succeed) {
-      if (res.body) {
-        try {
-          res.json = JSON.parse(res.body);
-        } catch (error) {}
-      }
-      delete config.retryList[uri];
-      return res;
-    } else {
-      return errorHandle(res.statusCode + ' ' + res.statusMessage);
-    }
-  } catch (error) {
-    if (error.cause && error.cause.errno === 'ETIMEDOUT' && error.cause.port === 443 && uri.match('http://')) {
-      option.uri = uri.replace('http://', 'https://');
-      return req(option, optionUser);
-    } else {
-      return errorHandle(error.message);
-    }
-  }
-}
+const req = require('./../_lib/req');
+req.config.init(config.req);
+require('./../_lib/log').hack();
 
 // Main
 let database;
+const doExit = (exit = true) => {
+  if (typeof database === 'object' && database instanceof Object) {
+    fs.writeFileSync('./database.json', JSON.stringify(database, (key, value) => {
+      if (typeof value === 'object' && value instanceof Array) {
+        return Array.from(new Set(value));
+      }
+      return value;
+    }, 2));
+  }
+  if (exit) process.exit();
+};
+const errorHandle = (res) => {
+  console.log(res.json);
+  if (res.json && res.json.message.match(/^API rate limit exceeded for/)) {
+    console.log(`Ratelimit Reset:\t${new Date(res.headers['x-ratelimit-reset'] * 1000).toLocaleString('zh-CN', { hour12: false })}`);
+    return true;
+  }
+};
 const main = async () => {
   if (fs.existsSync('./database.json')) {
     database = fs.readFileSync('database.json', 'utf-8');
     database = JSON.parse(database);
-    let lastUrl = [].concat(...Object.values(database.wait));
-    if (lastUrl.length === 0) {
-      console.error('None');
-      process.exit(-1);
-    } else {
-      lastUrl = lastUrl[0];
-    }
   } else {
     database = {
       wait: {
@@ -166,51 +57,99 @@ const main = async () => {
         repo: [],
         user: [],
         star: []
-      },
-      repo: []
+      }
     };
   }
-  ON_DEATH(function (signal, err) {
-    fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
-    process.exit();
-  });
 
-  while ([].concat(...Object.values(database.wait)).length) {
+  setInterval(() => {
+    doExit(false);
+  }, 1000 * 60 * 10);
+
+  while (Object.values(database.wait).flat().length) {
     if (database.wait.repo.length) {
-      const repo = database.wait.repo[0];
-      if (!database.done.repo.includes(repo)) {
-        const res = await req(`https://api.github.com/repos/${repo}/releases`);
-        if (!(res.json instanceof Array)) {
-          console.log(res.json);
-          if (res.json.message.match(/^API rate limit exceeded for/)) {
-            break;
-          }
-        } else if (res.json.filter(i => i.assets.length).length) {
-          database.repo.push(`https://github.com/${repo}/releases`);
-        }
+      const repos = database.wait.repo.slice(0, config.thread);
+      console.time('request');
+      const reses = await Promise.allSettled(repos.map(repo => {
+        if (database.done.repo.includes(repo)) return true;
+        return req(`https://api.github.com/repos/${repo}/releases`);
+      }));
+      console.timeEnd('request');
 
+      let exit = false;
+      for (let i = 0; i < repos.length; i++) {
+        if (reses[i].status === 'rejected' || reses[i].value === null) {
+          exit = true;
+          continue;
+        }
+        const repo = repos[i];
+        const res = reses[i].value;
+
+        if (typeof res === 'boolean') {
+        } else if (!(res.json instanceof Array)) {
+          console.log(repo);
+          if (errorHandle(res)) {
+            exit = true;
+            continue;
+          }
+        } else {
+          if (config.releaseFilter) {
+            if (res.json.some(config.releaseFilter)) fs.appendFileSync('repo.txt', `\n${repo}`);
+          } else {
+            fs.appendFileSync('repo.txt', `\n${repo}`);
+          }
+        }
         database.done.repo.push(repo);
+        if (database.wait.repo.includes(repo)) database.wait.repo.splice(database.wait.repo.indexOf(repo), 1);
       }
-      database.wait.repo.splice(0, 1);
+      if (exit) doExit();
     } else if (database.wait.user.length) {
       const user = database.wait.user[0];
-      if (!database.done.user.includes(user)) {
-        const res = await req(`https://api.github.com/users/${user}/repos`);
-        const repos = res.json.map(i => i.full_name);
-        database.wait.repo.push(...repos);
+      if (!(database.done.user.includes(user))) {
+        let page = 1;
+        while (true) {
+          const res = await req(`https://api.github.com/users/${user}/repos?sort=pushed&per_page=100&page=${page}`);
+          if (!(res.json instanceof Array)) {
+            if (errorHandle(res)) return;
+            break;
+          } else {
+            let repos = res.json;
+            if (config.repoFilter) repos = repos.filter(config.repoFilter);
+            database.wait.repo.push(...repos.map(i => i.full_name));
+            if (res.json.length >= 100) {
+              page++;
+            } else {
+              break;
+            }
+          }
+        }
 
         database.done.user.push(user);
       }
       database.wait.user.splice(0, 1);
     } else if (database.wait.star.length) {
       const user = database.wait.star[0];
-      if (!database.done.star.includes(user)) {
-        const res = await req(`https://api.github.com/users/${user}/starred`);
-        const repos = res.json.map(i => i.full_name);
-        database.wait.repo.push(...repos);
-        const users = res.json.map(i => i.owner.login);
-        database.wait.user.push(...users);
-        database.wait.star.push(...users);
+      if (!(database.done.star.includes(user))) {
+        let page = 1;
+        while (true) {
+          const res = await req(`https://api.github.com/users/${user}/starred?per_page=100&page=${page}`);
+          if (!(res.json instanceof Array)) {
+            if (errorHandle(res)) return;
+            break;
+          } else {
+            let repos = res.json;
+            if (config.repoFilter) repos = repos.filter(config.repoFilter);
+            database.wait.repo.push(...repos.map(i => i.full_name));
+
+            const users = res.json.map(i => i.owner.login);
+            database.wait.user.push(...users);
+            database.wait.star.push(...users);
+            if (res.json.length >= 100) {
+              page++;
+            } else {
+              break;
+            }
+          }
+        }
 
         database.done.star.push(user);
       }
@@ -219,12 +158,20 @@ const main = async () => {
   }
 };
 
-main().then(async () => {
-  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
-  //
-}, async err => {
-  fs.writeFileSync('./database.json', JSON.stringify(database, null, 2));
+try {
+  process.once('SIGINT', function () {
+    doExit(false);
+    console.log('SIGINT');
+    process.exit();
+  });
 
-  console.error(err);
-  process.exit();
-});
+  main().then(async () => {
+    doExit();
+  }, async err => {
+    console.error(err);
+
+    doExit();
+  });
+} catch (error) {
+  doExit();
+}
